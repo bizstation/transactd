b9cac14e136013e5808ae112cbcf8ab74c99e94a
BizStation <transactd@aie.ne.jp>
2014-06-20 11:43:07 +0900

    Add pooledDatabaseManager.h.

---
+++ source/bzs/db/protocol/tdap/client/pooledDatabaseManager.h
@@ -0,0 +1,127 @@
+#pragma once
+#ifndef	BZS_DB_PROTOCOL_TDAP_CLIENT_POOLEDDATABASEMANAGER_H
+#define	BZS_DB_PROTOCOL_TDAP_CLIENT_POOLEDDATABASEMANAGER_H
+
+/* =================================================================
+ Copyright (C) 20014 BizStation Corp All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ 02111-1307, USA.
+ ================================================================= */
+#include "trdboostapi.h"
+#include "connectionPool.h"
+namespace bzs
+{
+namespace db
+{
+namespace protocol
+{
+namespace tdap
+{
+namespace client
+{
+/*
+--------------------------------------
+pooledDbManager::setMaxConnections(n);
+
+pooledDbManager db;
+db.use(param)
+activeTable a(db, "user");
+....
+db.unUse();
+--------------------------------------
+Thread safe
+Method : non thread safe. 
+Object : thread safe.
+
+*/
+class pooledDbManager : public idatabaseManager
+{
+	dbmanager_ptr m_db;
+	bool m_inUse;
+	
+protected:
+	inline void connect(const connectParams& param, bool newConnection=false)
+	{
+		m_db->connect(param, newConnection);
+	}
+
+public:
+	inline pooledDbManager():m_inUse(false){};
+	
+	inline pooledDbManager(const connectParams* param):m_inUse(false)
+	{
+		use(param);
+	}
+	
+	inline ~pooledDbManager()
+	{
+		if (m_inUse)
+			unUse();
+	}
+
+	inline void use(const connectParams* param)
+	{
+		m_db = cpool.get(param);
+		m_inUse = true;
+	}
+	
+	inline void unUse()
+	{
+		m_db.reset();
+		releaseConnection(&cpool);
+		m_inUse = false;
+	}
+
+	inline table_ptr table(const _TCHAR* name){return m_db->table(name);}
+
+	inline table_ptr table(short index){return m_db->table(index);}
+
+	inline void setOption(__int64 v){m_db->setOption(v);};
+
+	inline __int64 option(){return m_db->option();};
+
+	inline void beginTrn(short bias){m_db->beginTrn(bias);};
+
+	inline void endTrn(){m_db->endTrn();}
+
+	inline void abortTrn(){m_db->abortTrn();}
+
+	inline int enableTrn(){return m_db->enableTrn();}
+
+	inline void beginSnapshot(){m_db->beginSnapshot();}
+
+	inline void endSnapshot(){m_db->endSnapshot();}
+
+	inline short_td stat() const {return m_db->stat();}
+
+	inline uchar_td* clientID() const{return m_db->clientID();}
+
+	inline const _TCHAR* uri() const{return m_db->uri();}
+
+	inline char_td mode() const{return m_db->mode();}
+
+	static void setMaxConnections(int maxWorkerNum){cpool.setMaxConnections(maxWorkerNum);};
+};
+
+
+}// namespace client
+}// namespace tdap
+}// namespace protocol
+}// namespace db
+}// namespace bzs
+
+#endif	//BZS_DB_PROTOCOL_TDAP_CLIENT_POOLEDDATABASEMANAGER_H
+
