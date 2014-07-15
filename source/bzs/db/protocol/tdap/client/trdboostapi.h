37e3eee4d2adcc7e7f42553ff027263f643c54d5
BizStation <transactd@aie.ne.jp>
2014-07-15 09:17:02 +0900

    Fix: Fix typo TCHAR to _TCHAR in trdboostapi.h.

--- source/bzs/db/protocol/tdap/client/trdboostapi.h
+++ source/bzs/db/protocol/tdap/client/trdboostapi.h
@@ -132,9 +132,9 @@ public:
 
 	query& reset(){queryBase::reset();return *this;}
 
-	query& select(const TCHAR* name, const TCHAR* name1=NULL, const TCHAR* name2=NULL, const TCHAR* name3=NULL
-				,const TCHAR* name4=NULL, const TCHAR* name5=NULL, const TCHAR* name6=NULL, const TCHAR* name7=NULL
-				,const TCHAR* name8=NULL, const TCHAR* name9=NULL, const TCHAR* name10=NULL)
+	query& select(const _TCHAR* name, const _TCHAR* name1=NULL, const _TCHAR* name2=NULL, const _TCHAR* name3=NULL
+				,const _TCHAR* name4=NULL, const _TCHAR* name5=NULL, const _TCHAR* name6=NULL, const _TCHAR* name7=NULL
+				,const _TCHAR* name8=NULL, const _TCHAR* name9=NULL, const _TCHAR* name10=NULL)
 	{
 		if (_tcscmp(name, _T("*"))==0)
 		{
