5bd329763260cdfbbb0d33d91bbee3009f6dec15
BizStation <transactd@aie.ne.jp>
2015-10-29 12:01:51 +0900

    Update: Increment release version. (2.4.5)

--- source/bzs/db/protocol/tdap/tdapcapi.h
+++ source/bzs/db/protocol/tdap/tdapcapi.h
@@ -504,7 +504,7 @@ struct handshale_t
  */
 #define C_INTERFACE_VER_MAJOR "2"//##1 Build marker! Don't remove
 #define C_INTERFACE_VER_MINOR "4"//##2 Build marker! Don't remove
-#define C_INTERFACE_VER_RELEASE "4"//##3 Build marker! Don't remove
+#define C_INTERFACE_VER_RELEASE "5"//##3 Build marker! Don't remove
 
 /* dnamic load library name.
  The default extention of Mac is ".boudle", Therefore ".so" is popular. */
@@ -568,7 +568,7 @@ struct handshale_t
 
 #define CPP_INTERFACE_VER_MAJOR "2"//##4 Build marker! Don't remove
 #define CPP_INTERFACE_VER_MINOR "4"//##5 Build marker! Don't remove
-#define CPP_INTERFACE_VER_RELEASE "4"//##6 Build marker! Don't remove
+#define CPP_INTERFACE_VER_RELEASE "5"//##6 Build marker! Don't remove
 
 /* use autolink tdclcpp */
 #if (__BCPLUSPLUS__ || _MSC_VER)
@@ -585,6 +585,6 @@ struct handshale_t
 
 #define TRANSACTD_VER_MAJOR 2//##7 Build marker! Don't remove
 #define TRANSACTD_VER_MINOR 4//##8 Build marker! Don't remove
-#define TRANSACTD_VER_RELEASE 4//##9 Build marker! Don't remove
+#define TRANSACTD_VER_RELEASE 5//##9 Build marker! Don't remove
 
 #endif // BZS_DB_PROTOCOL_TDAP_TDAPCAPI_H
