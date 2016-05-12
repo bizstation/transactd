5e1b5c2df6feef087f1346591244f2ae241c5362
BizStation <transactd@aie.ne.jp>
2016-05-12 17:20:11 +0900

    Update: Revision up to 3.4.0.

--- source/bzs/db/protocol/tdap/tdapcapi.h
+++ source/bzs/db/protocol/tdap/tdapcapi.h
@@ -625,7 +625,7 @@ struct handshale_t
  If you change this version then you need change The ($TargetName) project options too.
  */
 #define C_INTERFACE_VER_MAJOR "3"//##1 Build marker! Don't remove
-#define C_INTERFACE_VER_MINOR "3"//##2 Build marker! Don't remove
+#define C_INTERFACE_VER_MINOR "4"//##2 Build marker! Don't remove
 #define C_INTERFACE_VER_RELEASE "0"//##3 Build marker! Don't remove
 
 /* dnamic load library name.
@@ -689,7 +689,7 @@ struct handshale_t
  */
 
 #define CPP_INTERFACE_VER_MAJOR "3"//##4 Build marker! Don't remove
-#define CPP_INTERFACE_VER_MINOR "3"//##5 Build marker! Don't remove
+#define CPP_INTERFACE_VER_MINOR "4"//##5 Build marker! Don't remove
 #define CPP_INTERFACE_VER_RELEASE "0"//##6 Build marker! Don't remove
 
 /* use autolink tdclcpp */
@@ -726,7 +726,7 @@ struct handshale_t
 
 
 #define TRANSACTD_VER_MAJOR 3//##7 Build marker! Don't remove
-#define TRANSACTD_VER_MINOR 3//##8 Build marker! Don't remove
+#define TRANSACTD_VER_MINOR 4//##8 Build marker! Don't remove
 #define TRANSACTD_VER_RELEASE 0//##9 Build marker! Don't remove
 
 #endif // BZS_DB_PROTOCOL_TDAP_TDAPCAPI_H
